7e52d37fbd690a6f4fffe6c6bee3c70c74ccd058
galihos <Galih-OS@users.noreply.github.com>
2023-08-15 11:33:47 +0700

    Add files via upload

---
+++ config/server.php
@@ -0,0 +1,9 @@
+<?php
+// 1. Connect ke database
+$sqlconn=@mysql_connect("localhost","root","");
+
+// 2. Pilih database
+date_default_timezone_set("Asia/Jakarta");
+mysql_select_db("beesmartv3", $sqlconn);
+$mode = "lokal"; // pilih 'lokal' atau 'pusat'
+?>
